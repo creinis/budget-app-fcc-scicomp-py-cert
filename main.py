8b609ebb41e81f6497fe9c2dcc6e700c960c5736
creinis <115181527+creinis@users.noreply.github.com>
2024-08-02 18:16:23 +0200

    feat: The deposit method should append an object to the ledger list in the form of {"amount": amount, "description": description}.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Category:
         self.ledger = []
 
     def deposit(self, amount, description=""):
-        pass
+        self.ledger.append({"amount": amount, "description": description})
     
     def withdraw(self):
         pass
